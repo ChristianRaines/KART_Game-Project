7e52b4ade329b9c26785e0c4d41645c8d4fb0adf
网络巨人 <982801481@qq.com>
2021-01-05 14:47:14 +0800

    期末作业：卡丁车

---
+++ KART_PROJECT/Assets/CarTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTrigger : MonoBehaviour
+{
+    public GameObject falls;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("hello");
+            falls.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
---
+++ KART_PROJECT/Assets/fallscontroll.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallscontroll : MonoBehaviour
+{
+    public GameObject m_fall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "KartCollider")
+        {
+            m_fall.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
